69842ab9f2df2febf8cd47556cf7faa250a8ada8
Tanapat Phattharawat <oakko.dev@gmail.com>
2025-09-09 14:53:20 +0700

    Fix Vite/Prisma build issue with alias update
    Moved Prisma client alias configuration to Vite's resolve.alias using fileURLToPath for compatibility. This addresses build issues with Vite and Prisma integration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,20 @@
+import { fileURLToPath } from "node:url"
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+// const resolver = createResolver(import.meta.url)
+
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	modules: ["@prisma/nuxt"],
 	devtools: { enabled: true },
-	build: {
-		transpile: ["@prisma/client"],
-	},
-	alias: {
-		".prisma/client/index-browser": "@prisma/client",
-		".prisma/client/default": "@prisma/client",
+	vite: {
+		resolve: {
+			// fix for vite/prisma build issue
+			alias: {
+				".prisma/client/index-browser": fileURLToPath(
+					new URL("./node_modules/@prisma/client/index-browser.js", import.meta.url),
+				),
+			},
+		},
 	},
 })
